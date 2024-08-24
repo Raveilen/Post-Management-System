f4aab68bec47657507c46fe7f46851520af450b8
Raveilen <131012210+Raveilen@users.noreply.github.com>
2024-08-24 20:09:44 +0200

    and add source now

---
+++ src/PostManagementSystem/PostManagementSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PostManagementSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
