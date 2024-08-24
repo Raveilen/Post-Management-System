b1f2e609b4feb01a850de5d8f95c9db699992085
Raveilen <131012210+Raveilen@users.noreply.github.com>
2024-08-24 19:43:44 +0200

    aaa

---
+++ src/PostManagementSystem/PostManagementSystem/Models/Package.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PostManagementSystem.Models
+{
+    public class Package
+    {
+        public Guid ID { get; set; }
+
+        public int Weight { get; set; }
+        public string Dimensions { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime CreatedDate { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime UpdatedDate { get; set; }
+
+        public Customer Sender { get; set; }
+        public Customer Receiver { get; set; }
+        public Status Status { get; set; }
+    }
+}
